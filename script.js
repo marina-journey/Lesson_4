25c30b5d6809052beb5151a8030ba4a799779bb0
Marina Domrachova <apaycheva@gmail.com>
2020-07-13 00:58:19 +0300

    First try

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let randomNumber = Math.floor(Math.random() * 10);
+let guess = prompt('Я загадал число от 1 до 10! Попробуй отгадай. Какое число я загадал?');
+let numberOfGuess = 1;
 
+console.log(randomNumber);
+confirm('Хотите продолжить?')
+
+
+while (true) {
+    if (guess < randomNumber) {
+        guess = prompt('Мало. Попробуй еще раз!');
+        numberOfGuess = numberOfGuess + 1;  
+    } else if (guess > randomNumber) {
+        guess = prompt('Много. Попробуй еще раз!');
+        numberOfGuess = numberOfGuess + 1;
+    } else {
+        alert('Ты угадал! Поздравляю! Это число '+ randomNumber +'. Тебе понадобилось '+ numberOfGuess +' попыток.');
+        break;
+    }  
+ }  
